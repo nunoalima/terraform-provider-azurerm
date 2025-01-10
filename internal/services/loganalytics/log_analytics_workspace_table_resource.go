e683525da0e219772c6a26260c8633b131f6467c
Nuno Lima <nuno.lima@zerog.aero>
2025-01-10 19:56:27 +0000

    Extend azurerm_log_analytics_workspace_table resource for complete lifecycle management of workspace table

--- internal/services/loganalytics/log_analytics_workspace_table_resource.go
+++ internal/services/loganalytics/log_analytics_workspace_table_resource.go
@@ -6,7 +6,8 @@ package loganalytics
 import (
 	"context"
 	"fmt"
-	"log"
+	"regexp"
+	"strings"
 	"time"
 
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
@@ -16,7 +17,6 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
-	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
 
 type LogAnalyticsWorkspaceTableResource struct{}
@@ -26,12 +26,21 @@ var (
 	_ sdk.ResourceWithCustomizeDiff = LogAnalyticsWorkspaceTableResource{}
 )
 
+type TableColumn struct {
+	Name        string `tfschema:"name"`
+	Type        string `tfschema:"type"`
+	Description string `tfschema:"description"`
+	IsHidden    bool   `tfschema:"is_hidden"`
+}
+
 type LogAnalyticsWorkspaceTableResourceModel struct {
-	Name                 string `tfschema:"name"`
-	WorkspaceId          string `tfschema:"workspace_id"`
-	Plan                 string `tfschema:"plan"`
-	RetentionInDays      int64  `tfschema:"retention_in_days"`
-	TotalRetentionInDays int64  `tfschema:"total_retention_in_days"`
+	WorkspaceId          string        `tfschema:"workspace_id"`
+	Name                 string        `tfschema:"name"`
+	Description          string        `tfschema:"description"`
+	Plan                 string        `tfschema:"plan"`
+	RetentionInDays      int64         `tfschema:"retention_in_days"`
+	TotalRetentionInDays int64         `tfschema:"total_retention_in_days"`
+	Columns              []TableColumn `tfschema:"column"`
 }
 
 func (r LogAnalyticsWorkspaceTableResource) CustomizeDiff() sdk.ResourceFunc {
@@ -54,20 +63,63 @@ func (r LogAnalyticsWorkspaceTableResource) CustomizeDiff() sdk.ResourceFunc {
 func (r LogAnalyticsWorkspaceTableResource) Arguments() map[string]*pluginsdk.Schema {
 	return map[string]*pluginsdk.Schema{
 		"workspace_id": {
+			Description:  "The Workspace (or Customer) ID for the Log Analytics Workspace.",
 			Type:         pluginsdk.TypeString,
 			Required:     true,
+			ForceNew:     true,
 			ValidateFunc: workspaces.ValidateWorkspaceID,
 		},
 
 		"name": {
-			Type:     pluginsdk.TypeString,
-			Required: true,
+			Description:  "Specifies the name of the table in the Log Analytics Workspace. Must be suffixed with '_CL'.",
+			Type:         pluginsdk.TypeString,
+			Required:     true,
+			ForceNew:     true,
+			ValidateFunc: validateAlphanumericLengthAndSuffix,
+		},
+
+		"description": {
+			Description: "The description of the Log Analytics Workspace table.",
+			Type:        pluginsdk.TypeString,
+			Optional:    true,
+		},
+
+		"column": {
+			Description: "Defines the schema of a single column the table. Each column supports its 'name', 'type', 'description', and 'is_hidden' attributes.",
+			Type:        pluginsdk.TypeList,
+			Required:    true,
+			MinItems:    1,
+			Elem: &pluginsdk.Resource{
+				Schema: map[string]*pluginsdk.Schema{
+					"name": {
+						Description: "Specifies the name of the column.",
+						Type:        pluginsdk.TypeString,
+						Required:    true,
+					},
+					"type": {
+						Description: "Specifies the column type. For a list of supported types refer to the 'ColumnTypeEnum' type.",
+						Type:        pluginsdk.TypeString,
+						Required:    true,
+					},
+					"description": {
+						Description: "The description of the column.",
+						Type:        pluginsdk.TypeString,
+						Optional:    true,
+					},
+					"is_hidden": {
+						Description: "Specifies whether the column should be visible.",
+						Type:        pluginsdk.TypeBool,
+						Optional:    true,
+					},
+				},
+			},
 		},
 
 		"plan": {
-			Type:     pluginsdk.TypeString,
-			Optional: true,
-			Default:  string(tables.TablePlanEnumAnalytics),
+			Description: "Specifies the table plan type.",
+			Type:        pluginsdk.TypeString,
+			Optional:    true,
+			Default:     string(tables.TablePlanEnumAnalytics),
 			ValidateFunc: validation.StringInSlice([]string{
 				string(tables.TablePlanEnumAnalytics),
 				string(tables.TablePlanEnumBasic),
@@ -75,12 +127,14 @@ func (r LogAnalyticsWorkspaceTableResource) Arguments() map[string]*pluginsdk.Sc
 		},
 
 		"retention_in_days": {
+			Description:  "The table's retention period, in days.",
 			Type:         pluginsdk.TypeInt,
 			Optional:     true,
 			ValidateFunc: validation.IntBetween(4, 730),
 		},
 
 		"total_retention_in_days": {
+			Description:  "The table's total retention period, in days.",
 			Type:         pluginsdk.TypeInt,
 			Optional:     true,
 			ValidateFunc: validation.Any(validation.IntBetween(4, 730), validation.IntInSlice([]int{1095, 1460, 1826, 2191, 2556, 2922, 3288, 3653, 4018, 4383})),
@@ -108,38 +162,44 @@ func (r LogAnalyticsWorkspaceTableResource) Create() sdk.ResourceFunc {
 	return sdk.ResourceFunc{
 		Timeout: 5 * time.Minute,
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
-			var model LogAnalyticsWorkspaceTableResourceModel
-			if err := metadata.Decode(&model); err != nil {
-				return fmt.Errorf("decoding %+v", err)
-			}
 			client := metadata.Client.LogAnalytics.TablesClient
 
-			tableName := model.Name
-			log.Printf("[INFO] preparing arguments for AzureRM Log Analytics Workspace Table %s update.", tableName)
-
-			workspaceId, err := workspaces.ParseWorkspaceID(model.WorkspaceId)
-			if err != nil {
-				return fmt.Errorf("invalid workspace object ID for table %s: %s", tableName, err)
+			var inputModel LogAnalyticsWorkspaceTableResourceModel
+			if err := metadata.Decode(&inputModel); err != nil {
+				return fmt.Errorf("decoding %+v", err)
 			}
 
+			workspaceId, err := workspaces.ParseWorkspaceID(inputModel.WorkspaceId)
+			if err != nil {
+				return err
+			}
+
+			tableName := inputModel.Name
 			id := tables.NewTableID(workspaceId.SubscriptionId, workspaceId.ResourceGroupName, workspaceId.WorkspaceName, tableName)
 
-			updateInput := tables.Table{
+			existing, err := client.Get(ctx, id)
+			if err != nil && !response.WasNotFound(existing.HttpResponse) {
+				return fmt.Errorf("retrieving %s: %+v", id, err)
+			}
+			if !response.WasNotFound(existing.HttpResponse) {
+				return metadata.ResourceRequiresImport(r.ResourceType(), id)
+			}
+
+			table := tables.Table{
 				Properties: &tables.TableProperties{
-					Plan: pointer.To(tables.TablePlanEnum(model.Plan)),
+					Plan:                 pointer.To(tables.TablePlanEnum(inputModel.Plan)),
+					RetentionInDays:      pointer.To(inputModel.RetentionInDays),
+					TotalRetentionInDays: pointer.To(inputModel.TotalRetentionInDays),
+					Schema: &tables.Schema{
+						Name:        pointer.To(inputModel.Name),
+						Description: pointer.To(inputModel.Description),
+						Columns:     pointer.To(generateColumnsSchemaFromModel(inputModel.Columns)),
+					},
 				},
 			}
 
-			if model.Plan == string(tables.TablePlanEnumAnalytics) {
-				updateInput.Properties.RetentionInDays = pointer.To(model.RetentionInDays)
-			}
-
-			if model.TotalRetentionInDays != 0 {
-				updateInput.Properties.TotalRetentionInDays = pointer.To(model.TotalRetentionInDays)
-			}
-
-			if err := client.CreateOrUpdateThenPoll(ctx, id, updateInput); err != nil {
-				return fmt.Errorf("failed to update table %s in workspace %s in resource group %s: %s", tableName, workspaceId.WorkspaceName, workspaceId.ResourceGroupName, err)
+			if err := client.CreateOrUpdateThenPoll(ctx, id, table); err != nil {
+				return fmt.Errorf("creating %s: %+v", id, err)
 			}
 
 			metadata.SetID(id)
@@ -153,6 +213,7 @@ func (r LogAnalyticsWorkspaceTableResource) Update() sdk.ResourceFunc {
 		Timeout: 5 * time.Minute,
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
 			client := metadata.Client.LogAnalytics.TablesClient
+
 			id, err := tables.ParseTableID(metadata.ResourceData.Id())
 			if err != nil {
 				return err
@@ -165,37 +226,40 @@ func (r LogAnalyticsWorkspaceTableResource) Update() sdk.ResourceFunc {
 
 			existing, err := client.Get(ctx, *id)
 			if err != nil {
-				return fmt.Errorf("reading Log Analytics Workspace Table %s: %v", id, err)
+				return fmt.Errorf("retrieving %s: %+v", id, err)
 			}
 
-			if model := existing.Model; model != nil {
-				if props := model.Properties; props != nil {
-					updateInput := tables.Table{
-						Properties: &tables.TableProperties{
-							Plan: props.Plan,
-						},
-					}
+			model := existing.Model
+			if model == nil {
+				return fmt.Errorf("retrieving %s: `model` was nil", id)
+			}
+			if model.Properties == nil {
+				return fmt.Errorf("retrieving %s: `properties` was nil", id)
+			}
 
-					if metadata.ResourceData.HasChange("plan") {
-						updateInput.Properties.Plan = pointer.To(tables.TablePlanEnum(state.Plan))
-					}
+			if metadata.ResourceData.HasChange("description") {
+				model.Properties.Schema.Description = pointer.To(state.Description)
+			}
 
-					if state.Plan == string(tables.TablePlanEnumAnalytics) {
-						updateInput.Properties.RetentionInDays = existing.Model.Properties.RetentionInDays
+			if metadata.ResourceData.HasChange("plan") {
+				model.Properties.Plan = pointer.To(tables.TablePlanEnum(state.Plan))
+			}
 
-						if metadata.ResourceData.HasChange("retention_in_days") {
-							updateInput.Properties.RetentionInDays = pointer.To(state.RetentionInDays)
-						}
-					}
+			if state.Plan == string(tables.TablePlanEnumAnalytics) && metadata.ResourceData.HasChange("retention_in_days") {
+				model.Properties.RetentionInDays = pointer.To(state.RetentionInDays)
+			}
 
-					if metadata.ResourceData.HasChange("total_retention_in_days") {
-						updateInput.Properties.TotalRetentionInDays = pointer.To(state.TotalRetentionInDays)
-					}
+			if metadata.ResourceData.HasChange("total_retention_in_days") {
+				model.Properties.TotalRetentionInDays = pointer.To(state.TotalRetentionInDays)
+			}
 
-					if err := client.CreateOrUpdateThenPoll(ctx, *id, updateInput); err != nil {
-						return fmt.Errorf("failed to update table: %s: %+v", id.TableName, err)
-					}
-				}
+			if metadata.ResourceData.HasChange("column") {
+
+				model.Properties.Schema.Columns = pointer.To(generateColumnsSchemaFromModel(state.Columns))
+			}
+
+			if err := client.CreateOrUpdateThenPoll(ctx, *id, *model); err != nil {
+				return fmt.Errorf("updating %s: %+v", id.TableName, err)
 			}
 
 			return nil
@@ -207,38 +271,40 @@ func (r LogAnalyticsWorkspaceTableResource) Read() sdk.ResourceFunc {
 	return sdk.ResourceFunc{
 		Timeout: 5 * time.Minute,
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
+			client := metadata.Client.LogAnalytics.TablesClient
+
 			id, err := tables.ParseTableID(metadata.ResourceData.Id())
 			if err != nil {
-				return fmt.Errorf("while parsing resource ID: %+v", err)
+				return err
 			}
 
-			workspaceId, err := workspaces.ParseWorkspaceID(metadata.ResourceData.Get("workspace_id").(string))
+			workspaceId, err := workspaces.ParseWorkspaceID(extractWorkspaceIdFromTableId(id.ID()))
 			if err != nil {
-				return fmt.Errorf("while parsing resource ID: %+v", err)
+				return err
 			}
-
-			client := metadata.Client.LogAnalytics.TablesClient
 
 			resp, err := client.Get(ctx, *id)
 			if err != nil {
 				if response.WasNotFound(resp.HttpResponse) {
 					return metadata.MarkAsGone(id)
 				}
-				return fmt.Errorf("retrieving Log Analytics Workspace Table %s: %+v", *id, err)
+				return fmt.Errorf("retrieving %s: %+v", *id, err)
 			}
 
 			state := LogAnalyticsWorkspaceTableResourceModel{
-				Name:        id.TableName,
 				WorkspaceId: workspaceId.ID(),
+				Name:        id.TableName,
 			}
 
 			if model := resp.Model; model != nil {
 				if props := model.Properties; props != nil {
+					state.Description = pointer.From(props.Schema.Description)
+					state.Plan = string(pointer.From(props.Plan))
 					if pointer.From(props.Plan) == tables.TablePlanEnumAnalytics {
 						state.RetentionInDays = pointer.From(props.RetentionInDays)
 					}
 					state.TotalRetentionInDays = pointer.From(props.TotalRetentionInDays)
-					state.Plan = string(pointer.From(props.Plan))
+					state.Columns = generateColumnsModelFromSchema(pointer.From(props.Schema.Columns))
 				}
 			}
 
@@ -249,35 +315,72 @@ func (r LogAnalyticsWorkspaceTableResource) Read() sdk.ResourceFunc {
 
 func (r LogAnalyticsWorkspaceTableResource) Delete() sdk.ResourceFunc {
 	return sdk.ResourceFunc{
-		Timeout: 30 * time.Minute,
+		Timeout: 5 * time.Minute,
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
+			client := metadata.Client.LogAnalytics.TablesClient
+
 			var model LogAnalyticsWorkspaceTableResourceModel
 			if err := metadata.Decode(&model); err != nil {
 				return fmt.Errorf("decoding %+v", err)
 			}
-			client := metadata.Client.LogAnalytics.TablesClient
+
 			id, err := tables.ParseTableID(metadata.ResourceData.Id())
 			if err != nil {
-				return fmt.Errorf("while parsing resource ID: %+v", err)
+				return err
 			}
 
-			// We do not delete the resource here, just set the retention to workspace default value, which is
-			// achieved by setting the value to `-1`
-			retentionInDays := utils.Int64(-1)
-			totalRetentionInDays := utils.Int64(-1)
-
-			updateInput := tables.Table{
-				Properties: &tables.TableProperties{
-					RetentionInDays:      retentionInDays,
-					TotalRetentionInDays: totalRetentionInDays,
-				},
-			}
-
-			if err := client.CreateOrUpdateThenPoll(ctx, *id, updateInput); err != nil {
-				return fmt.Errorf("failed to update table %s in workspace %s in resource group %s: %s", id.TableName, id.WorkspaceName, id.ResourceGroupName, err)
+			if err := client.DeleteThenPoll(ctx, *id); err != nil {
+				return fmt.Errorf("deleting %s: %+v", id, err)
 			}
 
 			return nil
 		},
 	}
+}
+
+func validateAlphanumericLengthAndSuffix(val interface{}, key string) (warnings []string, errors []error) {
+	// From the Azure Portal: `Up to 45 alphanumeric characters`. In practice, it also supports underscores.
+	//  - Must not exceed 45 characters
+	//  - Must be suffixed with `_CL`
+	//  - Must only contain alphanumeric characters and underscores
+
+	tableName := val.(string)
+
+	if matched := regexp.MustCompile(`^[0-9a-zA-Z_]{1,42}_CL$`).Match([]byte(tableName)); !matched {
+		errors = append(errors, fmt.Errorf("%q table names must be suffixed with `_CL` and may only contain alphanumeric characters and underscores", key))
+	}
+
+	return warnings, errors
+}
+
+func generateColumnsSchemaFromModel(modelColumns []TableColumn) []tables.Column {
+	var schemaColumns []tables.Column
+	for _, column := range modelColumns {
+		schemaColumn := tables.Column{
+			Name:        pointer.FromString(column.Name),
+			Type:        pointer.To(tables.ColumnTypeEnum(column.Type)),
+			Description: pointer.To(column.Description),
+			IsHidden:    pointer.To(column.IsHidden),
+		}
+		schemaColumns = append(schemaColumns, schemaColumn)
+	}
+	return schemaColumns
+}
+
+func generateColumnsModelFromSchema(schemaColumns []tables.Column) []TableColumn {
+	var modelColumns []TableColumn
+	for _, column := range schemaColumns {
+		modelColumn := TableColumn{
+			Name:        pointer.From(column.Name),
+			Type:        strings.ToLower(string(*column.Type)),
+			Description: pointer.From(column.Description),
+			IsHidden:    pointer.From(column.IsHidden),
+		}
+		modelColumns = append(modelColumns, modelColumn)
+	}
+	return modelColumns
+}
+
+func extractWorkspaceIdFromTableId(tableId string) string {
+	return tableId[:strings.LastIndex(tableId, "/tables/")]
 }
--- internal/services/loganalytics/log_analytics_workspace_table_resource_test.go
+++ internal/services/loganalytics/log_analytics_workspace_table_resource_test.go
@@ -16,41 +16,106 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
 
-type LogAnalyticsWorkspaceTableResource struct{}
+type LogAnalyticsWorkspaceTableResourceTest struct{}
 
-func TestAccLogAnalyticsWorkspaceTable_updateTableRetention(t *testing.T) {
+func TestAccLogAnalyticsWorkspaceTable_basic(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_log_analytics_workspace_table", "test")
-	r := LogAnalyticsWorkspaceTableResource{}
+	r := LogAnalyticsWorkspaceTableResourceTest{}
 
 	data.ResourceTest(t, r, []acceptance.TestStep{
 		{
-			Config: r.updateRetention(data),
+			Config: r.basic(data),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
-				check.That(data.ResourceName).Key("id").Exists(),
-				check.That(data.ResourceName).Key("name").HasValue("AppEvents"),
-				check.That(data.ResourceName).Key("retention_in_days").HasValue("7"),
-				check.That(data.ResourceName).Key("total_retention_in_days").HasValue("32"),
+				check.That(data.ResourceName).Key("name").HasValue("acctestLAWTable_CL"),
+				check.That(data.ResourceName).Key("column.#").HasValue("1"),
+				check.That(data.ResourceName).Key("column.0.name").HasValue("TimeGenerated"),
+				check.That(data.ResourceName).Key("column.0.type").HasValue("datetime"),
 			),
 		},
+		data.ImportStep(),
 	})
 }
 
-func TestAccLogAnalyticsWorkspaceTable_plan(t *testing.T) {
+func TestAccLogAnalyticsWorkspaceTable_complete(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_log_analytics_workspace_table", "test")
-	r := LogAnalyticsWorkspaceTableResource{}
+	r := LogAnalyticsWorkspaceTableResourceTest{}
 
 	data.ResourceTest(t, r, []acceptance.TestStep{
 		{
-			Config: r.plan(data),
+			Config: r.complete(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("name").HasValue("acctestLAWTable_CL"),
+				check.That(data.ResourceName).Key("description").HasValue("A custom log table"),
+				check.That(data.ResourceName).Key("plan").HasValue("Analytics"),
+				check.That(data.ResourceName).Key("retention_in_days").HasValue("15"),
+				check.That(data.ResourceName).Key("total_retention_in_days").HasValue("60"),
+				check.That(data.ResourceName).Key("column.#").HasValue("8"),
+				check.That(data.ResourceName).Key("column.0.name").HasValue("TimeGenerated"),
+				check.That(data.ResourceName).Key("column.0.type").HasValue("datetime"),
+				check.That(data.ResourceName).Key("column.0.description").HasValue("A description for TimeGenerated"),
+				check.That(data.ResourceName).Key("column.0.is_hidden").HasValue("true"),
+				check.That(data.ResourceName).Key("column.7.name").HasValue("Mux"),
+				check.That(data.ResourceName).Key("column.7.type").HasValue("string"),
+				check.That(data.ResourceName).Key("column.7.description").HasValue("A description for Mux"),
+				check.That(data.ResourceName).Key("column.7.is_hidden").HasValue("false"),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func TestAccLogAnalyticsWorkspaceTable_requiresImport(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_log_analytics_workspace_table", "test")
+	r := LogAnalyticsWorkspaceTableResourceTest{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
 			),
 		},
+		data.RequiresImportErrorStep(r.requiresImport),
 	})
 }
 
-func (t LogAnalyticsWorkspaceTableResource) Exists(ctx context.Context, clients *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
+func TestAccLogAnalyticsWorkspaceTable_update(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_log_analytics_workspace_table", "test")
+	r := LogAnalyticsWorkspaceTableResourceTest{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("description").IsEmpty(),
+				check.That(data.ResourceName).Key("retention_in_days").HasValue("30"),
+				check.That(data.ResourceName).Key("total_retention_in_days").HasValue("30"),
+				check.That(data.ResourceName).Key("column.#").HasValue("1"),
+				check.That(data.ResourceName).Key("column.0.description").IsEmpty(),
+				check.That(data.ResourceName).Key("column.0.is_hidden").HasValue("false"),
+			),
+		},
+		data.ImportStep(),
+		{
+			Config: r.complete(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+				check.That(data.ResourceName).Key("description").HasValue("A custom log table"),
+				check.That(data.ResourceName).Key("retention_in_days").HasValue("15"),
+				check.That(data.ResourceName).Key("total_retention_in_days").HasValue("60"),
+				check.That(data.ResourceName).Key("column.#").HasValue("8"),
+				check.That(data.ResourceName).Key("column.0.description").HasValue("A description for TimeGenerated"),
+				check.That(data.ResourceName).Key("column.0.is_hidden").HasValue("true"),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func (t LogAnalyticsWorkspaceTableResourceTest) Exists(ctx context.Context, clients *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
 	id, err := tables.ParseTableID(state.ID)
 	if err != nil {
 		return nil, err
@@ -58,56 +123,144 @@ func (t LogAnalyticsWorkspaceTableResource) Exists(ctx context.Context, clients 
 
 	resp, err := clients.LogAnalytics.TablesClient.Get(ctx, *id)
 	if err != nil {
-		return nil, fmt.Errorf("reading Log Analytics Workspace Table (%s): %+v", id.ID(), err)
+		return nil, fmt.Errorf("reading workspace table (%s): %+v", id.ID(), err)
 	}
 
 	return utils.Bool(resp.Model.Id != nil), nil
 }
 
-func (LogAnalyticsWorkspaceTableResource) updateRetention(data acceptance.TestData) string {
+func (r LogAnalyticsWorkspaceTableResourceTest) basic(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 provider "azurerm" {
   features {}
 }
+
 resource "azurerm_resource_group" "test" {
   name     = "acctestRG-%d"
   location = "%s"
 }
+
 resource "azurerm_log_analytics_workspace" "test" {
   name                = "acctestLAW-%d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-  retention_in_days   = 30
 }
+
 resource "azurerm_log_analytics_workspace_table" "test" {
-  name                    = "AppEvents"
-  workspace_id            = azurerm_log_analytics_workspace.test.id
-  retention_in_days       = 7
-  total_retention_in_days = 32
+  name					  = "acctestLAWTable_CL"
+  description			  = ""
+  workspace_id			  = azurerm_log_analytics_workspace.test.id
+  retention_in_days 	  = 30
+  total_retention_in_days = 30
+
+  column {
+    name = "TimeGenerated"
+    type = "datetime"
+  }
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
 }
 
-func (LogAnalyticsWorkspaceTableResource) plan(data acceptance.TestData) string {
+func (r LogAnalyticsWorkspaceTableResourceTest) complete(data acceptance.TestData) string {
 	return fmt.Sprintf(`
 provider "azurerm" {
   features {}
 }
+
 resource "azurerm_resource_group" "test" {
   name     = "acctestRG-%d"
   location = "%s"
 }
+
 resource "azurerm_log_analytics_workspace" "test" {
   name                = "acctestLAW-%d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-  retention_in_days   = 30
 }
+
 resource "azurerm_log_analytics_workspace_table" "test" {
-  name                    = "AppTraces"
+  name                    = "acctestLAWTable_CL"
+  description			  = "A custom log table"
   workspace_id            = azurerm_log_analytics_workspace.test.id
-  plan                    = "Basic"
-  total_retention_in_days = 32
+  plan                    = "Analytics"
+  retention_in_days		  = 15
+  total_retention_in_days = 60
+
+  column {
+    name		= "TimeGenerated"
+    type		= "datetime"
+	description = "A description for TimeGenerated"
+	is_hidden	= true
+  }
+
+  column {
+    name		= "Foo"
+    type		= "boolean"
+	description = "A description for Foo"
+	is_hidden	= false
+  }
+
+  column {
+    name		= "Bar"
+    type		= "int"
+	description = "A description for Bar"
+	is_hidden	= false
+  }
+
+  column {
+    name		= "Baz"
+    type		= "long"
+	description = "A description for Baz"
+	is_hidden	= false
+  }
+
+  column {
+    name		= "Qux"
+    type		= "dynamic"
+	description = "A description for Qux"
+	is_hidden	= false
+  }
+
+  column {
+    name		= "Cor"
+    type		= "guid"
+	description = "A description for Cor"
+	is_hidden	= false
+  }
+
+  column {
+    name		= "Zap"
+    type		= "real"
+	description = "A description for Zap"
+	is_hidden	= false
+  }
+
+  column {
+    name		= "Mux"
+    type		= "string"
+	description = "A description for Mux"
+	is_hidden	= false
+  }
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger)
+}
+
+func (r LogAnalyticsWorkspaceTableResourceTest) requiresImport(data acceptance.TestData) string {
+	template := r.basic(data)
+	return fmt.Sprintf(`
+%[1]s
+
+resource "azurerm_log_analytics_workspace_table" "import" {
+  name					  = "acctestLAWTable_CL"
+  description			  = ""
+  workspace_id			  = azurerm_log_analytics_workspace.test.id
+  retention_in_days 	  = 30
+  total_retention_in_days = 30
+
+  column {
+    name = "TimeGenerated"
+    type = "datetime"
+  }
+}
+`, template)
 }
